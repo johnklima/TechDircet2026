4a349f6166c84c13e8684ed25ea659c6f29b0b52
John <klima.de.sa@gmail.com>
2026-04-09 19:07:38 +0200

    crazy chocos

--- LunarLander2026/Assets/Scripts/ChocoVFX.cs
+++ LunarLander2026/Assets/Scripts/ChocoVFX.cs
@@ -7,21 +7,30 @@ public class ChocoVFX : MonoBehaviour
 {
     public VisualEffect choco;
     public VisualEffect gas;
+    public VisualEffect disolve; 
 
     private void Start()
     {
        choco.pause = true;
        gas.pause = true;
+       disolve.pause = true;
     }
 
     public void PlayChoco()
     {
         choco.Reinit();
         gas.Reinit();
+        disolve.Reinit();
         choco.pause = false;
         gas.pause = false;
+        disolve.pause= false;   
+       
+
+
         StartCoroutine(stopVfx(choco, 1));
         StartCoroutine(stopVfx(gas, 1.5f));
+
+        //StartCoroutine(stopVfx(disolve, 1.5f));
 
     }
     IEnumerator stopVfx(VisualEffect effect, float time)
