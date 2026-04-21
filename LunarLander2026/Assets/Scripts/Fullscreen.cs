d51fd15d91e59d5e8cb2d5fdb68210e83f59cdaa
John <klima.de.sa@gmail.com>
2026-04-21 12:21:17 +0200

    organize

---
+++ LunarLander2026/Assets/Scripts/Fullscreen.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Fullscreen : MonoBehaviour
+{
+    public Material fullscreen;
+    public bool fade = false;
+    private float fadeVal = 0;
+
+    private void Start()
+    {
+        fullscreen.SetFloat("_VignetteIntensity", 0);
+        fade = false;
+        fadeVal = 0;
+    }
+    private void Update()
+    {
+        if(fade)
+        {
+            fadeVal += Time.deltaTime * 3.0f;
+            
+            if(fadeVal > 10 )
+            {
+                fadeVal = 10;
+                fade = false;
+            }
+            Fade(fadeVal);
+        }
+    }
+
+    public void Fade(float amount)
+    {
+        fullscreen.SetFloat("_VignetteIntensity", amount);
+    }
+
+}
